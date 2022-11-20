4ecbb5c4186ffec21bafeae712ec16502b4a93aa
aad3365 <aad336545@gmail.com>
2022-11-21 01:11:33 +0900

    add etherscan, verify contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,22 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
 require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, API_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
-   solidity: "0.7.3",
-   defaultNetwork: "goerli",
+   solidity: '0.7.3',
+   defaultNetwork: 'goerli',
    networks: {
       hardhat: {},
       goerli: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`],
-      },
+         url: API_URL + API_KEY,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
    },
+   etherscan: {
+      apiKey: ETHERSCAN_API_KEY
+   }
 };
